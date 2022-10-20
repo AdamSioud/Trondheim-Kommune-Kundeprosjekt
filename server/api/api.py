c80d368b9e636150dbe9599a68d108bec1aca0f8
evaboen <79899109+evaboen@users.noreply.github.com>
2022-10-20 16:38:45 +0200

    Delete server/api directory

--- server/api/api.py
+++
@@ -1,3 +0,0 @@
-# server — folder that contains all the routes/end points of the application.
-# These routes/end points only for accept the request. 
-# The logic of this routes/end points are implemented in a different folder.
